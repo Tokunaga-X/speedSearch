d3b55dff0b3730529fa28af61016567f9d7749b2
TokunagaX <officialxyc@gmail.com>
2024-04-25 23:57:29 +0800

    Create option.js

---
+++ option.js
@@ -0,0 +1,12 @@
+// Add event listener to all radio inputs, update the index,value,checked in chrome.storage
+const radioInputs = document.querySelectorAll('input[type="radio"]')
+for (let i in radioInputs) {
+    radioInputs[i].addEventListener("change", function () {
+        chrome.storage.sync.get("OptionList", ({ OptionList }) => {
+            OptionList[this.value] = this.checked
+            chrome.storage.sync.set({ OptionList })
+        })
+    })
+}
+
+// input[type="radio"] change event listener
