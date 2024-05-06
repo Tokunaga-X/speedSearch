6898a80d0a85b696be7c382d2247b89eb04ddb9a
TokunagaX <officialxyc@gmail.com>
2024-05-06 23:07:17 +0800

    feat: upgrade manifest to version 3 since 2 will be depated

--- popup.js
+++ popup.js
@@ -1,85 +1,97 @@
 "use strict"
 
-export const OPTIONS = [
-    { value: "google", text: "谷歌" },
-    { value: "baidu", text: "百度" },
-    { value: "zhihu", text: "知乎" },
-    { value: "douban", text: "豆瓣" },
-    { value: "bilibili", text: "B站" },
-    { value: "taobao", text: "淘宝" },
-]
+// export const OPTIONS = [
+//     { value: "google", text: "谷歌" },
+//     { value: "baidu", text: "百度" },
+//     { value: "zhihu", text: "知乎" },
+//     { value: "douban", text: "豆瓣" },
+//     { value: "bilibili", text: "B站" },
+//     { value: "taobao", text: "淘宝" },
+// ]
 
-// chrome.storage.sync.get("OptionList", ({ OptionList }) => {
-//     OptionList.forEach((item, index) => {
-//         OPTIONS[index].checked = item.checked
-//     })
-// })
+// // chrome.storage.sync.get("OptionList", ({ OptionList }) => {
+// //     OptionList.forEach((item, index) => {
+// //         OPTIONS[index].checked = item.checked
+// //     })
+// // })
 
-const select = document.getElementById("select")
+// const select = document.getElementById("select")
 
-for (let i = 0; i < OPTIONS.length; i++) {
-    if (!OPTIONS[i].checked) continue
-    const option = document.createElement("option")
-    option.value = OPTIONS[i].value
-    option.text = OPTIONS[i].text
-    select.appendChild(option)
-}
+// for (let i = 0; i < OPTIONS.length; i++) {
+//     // if (!OPTIONS[i].checked) continue
+//     const option = document.createElement("option")
+//     option.value = OPTIONS[i].value
+//     option.text = OPTIONS[i].text
+//     select.appendChild(option)
+// }
 
-var btn = document.getElementById("btn")
+var searchInputElement = document.getElementById("input")
+var searchButtonElement = document.getElementById("btn")
+var searchSelectElement = document.getElementById("select")
+
 //设置打开页面光标就聚焦在输入栏
 window.onload = function () {
-    document.getElementById("input").focus()
+    searchInputElement.focus()
 }
-//搜索功能实现
-btn.onclick = function () {
-    var text = document.getElementById("input")
-    if (input.value == "") {
-        console.log("empty!")
-    } else {
-        var select = document.getElementById("select")
-        switch (select.value) {
-            case "zhihu":
-                window.open(
-                    "https://www.zhihu.com/search?type=content&q=" +
-                        encodeURI(input.value)
-                )
-                break
-            case "douban":
-                window.open(
-                    "https://www.douban.com/search?source=suggest&q=" +
-                        encodeURI(input.value)
-                )
-                break
-            case "bilibili":
-                window.open(
-                    "https://search.bilibili.com/all?keyword=" +
-                        encodeURI(input.value)
-                )
-                break
-            case "taobao":
-                window.open(
-                    "https://s.taobao.com/search?q=" + encodeURI(input.value)
-                )
-                break
-            case "google":
-                window.open(
-                    "https://www.google.com/search?q=" + encodeURI(input.value)
-                )
-                break
-            case "baidu":
-                window.open(
-                    "https://www.baidu.com/s?wd=" + encodeURI(input.value)
-                )
-                break
-            default:
-                console.log("empty!")
-        }
-    }
-}
+
 //设置回车等于点击按钮
-document.getElementById("input").addEventListener("keyup", function (event) {
+searchInputElement.addEventListener("keyup", function (event) {
     event.preventDefault()
-    if (event.keyCode == 13) {
-        document.getElementById("btn").click()
+    if (event.keyCode === 13) {
+        searchButtonElement.click()
     }
 })
+
+//搜索功能实现
+searchButtonElement.onclick = function () {
+    if (searchInputElement.value == "") {
+        console.log("Search input is empty!")
+        return
+    }
+
+    switch (searchSelectElement.value) {
+        case "zhihu":
+            openSearchPage(
+                "https://www.zhihu.com/search?type=content&q=",
+                searchInputElement.value
+            )
+            break
+        case "douban":
+            window.open(
+                "https://www.douban.com/search?source=suggest&q=" +
+                    encodeURI(searchInputElement.value)
+            )
+            break
+        case "bilibili":
+            window.open(
+                "https://search.bilibili.com/all?keyword=" +
+                    encodeURI(searchInputElement.value)
+            )
+            break
+        case "taobao":
+            window.open(
+                "https://s.taobao.com/search?q=" +
+                    encodeURI(searchInputElement.value)
+            )
+            break
+        case "google":
+            window.open(
+                "https://www.google.com/search?q=" +
+                    encodeURI(searchInputElement.value)
+            )
+            break
+        case "baidu":
+            window.open(
+                "https://www.baidu.com/s?wd=" +
+                    encodeURI(searchInputElement.value)
+            )
+            break
+        default:
+            console.log("empty!")
+    }
+}
+
+function openSearchPage(baseUrl, query) {
+    var searchURL = baseUrl + encodeURIComponent(query)
+    chrome.tabs.create({ url: searchURL })
+}
